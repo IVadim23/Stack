fec2bf4e3c0f76e603bdf79e129164a89ad8340c
IVadim23 <inazrski@gmail.com>
2024-09-04 15:11:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Stack
+{
+    public class ArrStack<T>
+    {
+        private T[] elements;
+        public int Count { get; private set; }
+        private const int InitialCapacity = 16;
+        public ArrStack(int capacity = InitialCapacity)
+        {
+            this.elements = new T[capacity];
+            this.Count = Count;
+        }
+        public void Push(T element)
+        {
+            if (this.Count == this.elements.Length)
+            {
+                this.Grow();
+            }
+            this.elements[this.Count] = element;
+            this.Count++;
+        }
+        private void Grow()
+        {
+            T[] NewElements = new T[2 * this.elements.Length];
+            for (int i = 0; i < this.Count; i++)
+            {
+                NewElements[i] = this.elements[i];
+            }
+            elements = NewElements;
+        }
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+            this.Count--;
+            return this.elements[this.Count];
+        }
+        public T[] ToArray ()
+        {
+            T [] arr = new T[this.Count];
+            for (int i = 0;i < this.Count;i++)
+            {
+                arr[i]= this.elements[i];
+            }
+            return arr;
+        }
+    }
+}
